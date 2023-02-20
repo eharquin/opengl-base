c047d30520863a70d766e286295b7649c5af8c75
harqenzo <e.harquin@outlook.fr>
2023-02-20 17:07:41 +0100

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@ const int HEIGHT = 400;
 
 int main()
 {
+	// init GLFW, add opengl version and use core profile (modern opengl)
 	glfwInit();
-
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// create window (define the viewport by default)
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OPENGLBASE", NULL, NULL);
 	if (window == NULL)
 	{
@@ -18,27 +19,37 @@ int main()
 		throw std::runtime_error("Failed to create the GLFW window");
 	}
 
+	// introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
-	if (!gladLoadGLLoader(GLADloadproc(glfwGetProcAddress)))
+	// load glad 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 		throw std::runtime_error("Failed to use glad loader");
 
-
+	// define callback to window resizing
 	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
 	{
-		glViewport(0, 0, width, height);
+			// specify the viewport : "size of the screen"
+			std::cout << "test";
+			glViewport(0, 0, width, height);
 	});
 
+	// define the clear color (VIOLET)
 	glClearColor(.3, .1, .4, 1.);
 
 	while (!glfwWindowShouldClose(window))
-	{
+	{	
+		// clear the screen with the clear color
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// compute events of the window
 		glfwPollEvents();
+
+		// swap the two buffers (Front -> Back | Back -> Front)
 		glfwSwapBuffers(window);
 	}
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
