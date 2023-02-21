830580d614872a113c1a94b7548a13a00ed8107b
harqenzo <e.harquin@outlook.fr>
2023-02-21 19:54:42 +0100

    correct aspect ratio

--- src/main.cpp
+++ src/main.cpp
@@ -211,7 +211,7 @@ int main()
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 		glm::mat4 projection(1.0f);
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+		projection = glm::perspective(glm::radians(45.0f), float(WIDTH) / HEIGHT, 0.1f, 100.0f);
 
 		glm::mat4 mvp = projection * view * model;
 
