ec81c24af62f28d9ee5d23bf10f94219b88a589e
harqenzo <e.harquin@outlook.fr>
2023-02-21 12:18:25 +0100

    add simple triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,39 @@
 #include "openglbase.h"
 
 const int WIDTH = 800;
-const int HEIGHT = 400;
+const int HEIGHT = 800;
+
+const unsigned short OPENGL_MAJOR_VERSION = 4;
+const unsigned short OPENGL_MINOR_VERSION = 6;
+
+const char* vertexShaderSource =
+"#version 460 core\n"
+"layout (location = 0) in vec2 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(.4f, 0.3f, 0.7f, 1.0f);\n"
+"}\n";
 
 int main()
 {
+	// ---------------------------------------------------------------------------------
+	// glfw, context and glad initialisation
+
 	// init GLFW, add opengl version and use core profile (modern opengl)
+
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPENGL_MAJOR_VERSION);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPENGL_MINOR_VERSION);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 	// create window (define the viewport by default)
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OPENGLBASE", NULL, NULL);
@@ -21,6 +45,7 @@ int main()
 
 	// introduce the window into the current context
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(true);
 
 	// load glad 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -28,18 +53,99 @@ int main()
 
 	// define callback to window resizing
 	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
+		{
+			// specify the viewport : "size of the screen"
+			glViewport(0, 0, width, height);
+		});
+	// ---------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------
+
+
+	// ---------------------------------------------------------------------------------
+	// create vertexshader, fragmentshader, compile them and link to the shader program
+	GLuint fragmentShader, vertexShader, shaderProgram;
+	int success;
+	char infoLog[512];
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
 	{
-		// specify the viewport : "size of the screen"
-		glViewport(0, 0, width, height);
-	});
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
-	// define the clear color (VIOLET)
-	glClearColor(.3, .1, .4, 1.);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
 
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_ERROR\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	// ---------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------
+
+	std::vector<glm::vec2> vertices = {
+		{.0f, .5f },
+		{-.5f, -.5f },
+		{.5f, -.5f },
+	};
+
+	// -----------------------------------------------------------------------------------
+	// create vertex buffer object and vertex arrays
+	GLuint VBO, VAO;
+
+	// create vertex buffer object to store raw vertices
+	glCreateBuffers(1, &VBO);
+	glNamedBufferData(VBO, vertices.size() * sizeof(glm::vec2), vertices.data(), GL_STATIC_DRAW);
+
+	// create vertex array to store the organized vertices
+	glCreateVertexArrays(1, &VAO);
+
+
+	glVertexArrayVertexBuffer(VAO, 0, VBO, 0, 2 * sizeof(float));
+
+	glEnableVertexArrayAttrib(VAO, 0);
+	glVertexArrayAttribFormat(VAO, 0, 2, GL_FLOAT, false, 0);
+	glVertexArrayAttribBinding(VAO, 0, 0);
+	// -----------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------
+
+
+	// define the clear color (clear blue)
+	glClearColor(.6f, .6f, .8f, 1.f);
+
+
+	// -----------------------------------------------------------------------------------
+	// render loop
 	while (!glfwWindowShouldClose(window))
-	{	
+	{
 		// clear the screen with the clear color
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
 		// compute events of the window
 		glfwPollEvents();
@@ -47,6 +153,10 @@ int main()
 		// swap the two buffers (Front -> Back | Back -> Front)
 		glfwSwapBuffers(window);
 	}
+	// -----------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------
+
+
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
