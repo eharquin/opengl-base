b233fd75f06a2769ad28a5519c6537c703f48651
harqenzo <e.harquin@outlook.fr>
2023-03-03 20:54:30 +0100

    add diffuse map, spacular map and emission map

--- shader/global_fragment.glsl
+++ shader/global_fragment.glsl
@@ -2,9 +2,9 @@
 
 struct Material
 {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emission;
     float shininess;
 };
 uniform Material material;
@@ -19,28 +19,37 @@ uniform Light light;
 
 uniform vec3 viewPos;
 
-in vec3 vNormal;
 in vec3 vPos;
+in vec3 vNormal;
+in vec2 vTexCoords;
 
 out vec4 FragColor;
 
 void main()
 {
     // ambient
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, vTexCoords));
 
     // diffuse
     vec3 normal = normalize(vNormal);
     vec3 lightDir = normalize(vPos - light.position);
     float diff = max(dot(normal, -lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, vTexCoords));
 
     // specular
 	vec3 viewDir = normalize(viewPos - vPos);
     vec3 reflectDir = reflect(lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, vTexCoords));
+
+    // emission
+    vec3 emission = vec3(0.0);
+    if(vec3(texture(material.specular, vTexCoords)) == vec3(0.0, 0.0, 0.0))
+    {
+        emission = vec3(texture(material.emission, vTexCoords));
+    }
+
     
-    vec3 result = ambient + diffuse + specular;
+    vec3 result = ambient + diffuse + specular + emission;
     FragColor = vec4(result, 1.0);
 }
--- shader/global_vertex.glsl
+++ shader/global_vertex.glsl
@@ -6,13 +6,16 @@ uniform mat4 projection;
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 out vec3 vNormal;
 out vec3 vPos;
+out vec2 vTexCoords;
 
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
 	vNormal = mat3(transpose(inverse(model))) * aNormal;
 	vPos = vec3(model * vec4(aPos, 1.0));
+	vTexCoords = aTexCoords;
 }
