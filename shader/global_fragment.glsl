60dc785a037ac0e3ce33fa6486c0fd368524ccac
harqenzo <e.harquin@outlook.fr>
2023-03-03 16:55:52 +0100

    implement Gouraud basic lighting

--- shader/global_fragment.glsl
+++ shader/global_fragment.glsl
@@ -1,31 +1,12 @@
 #version 460 core
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
-
-in vec3 vNormal;
-in vec3 vPos;
+in vec3 vColor;
 
 out vec4 FragColor;
 
 void main()
 {
-    float ambientStrength = 0.2;
-    vec3 ambient = ambientStrength * lightColor;
-
-    vec3 normal = normalize(vNormal);
-    vec3 lightDir = normalize(vPos - lightPos);
-    float diff = max(dot(normal, -lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    float specularStrength = 0.5;
-	vec3 viewDir = normalize(viewPos - vPos);
-    vec3 reflectDir = reflect(lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
-    vec3 specular = specularStrength * spec * lightColor;  
-    
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(vColor, 1.0);
 }
+
+
--- shader/global_vertex.glsl
+++ shader/global_vertex.glsl
@@ -4,15 +4,38 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
-out vec3 vNormal;
-out vec3 vPos;
+out vec3 vColor;
 
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	vNormal = mat3(transpose(inverse(model))) * aNormal;
-	vPos = vec3(model * vec4(aPos, 1.0));
+	vec3 worldSpaceNormalNormalize = normalize(mat3(transpose(inverse(model))) * aNormal);
+	vec3 worldSpacePosition = vec3(model * vec4(aPos, 1.0));
+
+	// ambient light
+	float ambientLightStrength = 0.2;
+	vec3 ambient = ambientLightStrength * lightColor;
+
+	// diffuse light
+	vec3 lightDirectionNormalize = normalize(worldSpacePosition - lightPos);
+	float diffuseLightStrenght = max(dot(worldSpaceNormalNormalize, -lightDirectionNormalize), 0.0);
+	vec3 diffuse = diffuseLightStrenght * lightColor;
+
+	// specular light
+	int specularShininess = 64;
+	float specularStrength = 0.5;
+	vec3 viewDirectionNormalize = normalize(worldSpacePosition - viewPos);
+	vec3 reflectLightDirectionNormalize = reflect(lightDirectionNormalize, worldSpaceNormalNormalize);
+	float specularLightStrength = pow(max(dot(-viewDirectionNormalize, reflectLightDirectionNormalize), 0.0), specularShininess);
+	vec3 specular = specularStrength * specularLightStrength * lightColor;
+
+    vColor = (ambient + diffuse + specular) * objectColor;
 }
