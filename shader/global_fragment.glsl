b261416b799f03184e9a95a7a0adbe5edc59b2da
harqenzo <e.harquin@outlook.fr>
2023-03-03 17:11:38 +0100

    implement lighting calculation in view space

--- shader/global_fragment.glsl
+++ shader/global_fragment.glsl
@@ -2,11 +2,10 @@
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
 
 in vec3 vNormal;
 in vec3 vPos;
+in vec3 vLightPos; // in view space
 
 out vec4 FragColor;
 
@@ -16,12 +15,12 @@ void main()
     vec3 ambient = ambientStrength * lightColor;
 
     vec3 normal = normalize(vNormal);
-    vec3 lightDir = normalize(vPos - lightPos);
+    vec3 lightDir = normalize(vPos - vLightPos);
     float diff = max(dot(normal, -lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
     float specularStrength = 0.5;
-	vec3 viewDir = normalize(viewPos - vPos);
+	vec3 viewDir = normalize(-vPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
     vec3 reflectDir = reflect(lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
     vec3 specular = specularStrength * spec * lightColor;  
--- shader/global_vertex.glsl
+++ shader/global_vertex.glsl
@@ -4,15 +4,19 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+uniform vec3 lightPos;
+
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
 out vec3 vNormal;
 out vec3 vPos;
+out vec3 vLightPos; // in view space
 
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	vNormal = mat3(transpose(inverse(model))) * aNormal;
-	vPos = vec3(model * vec4(aPos, 1.0));
+	vNormal = mat3(transpose(inverse(view * model))) * aNormal;
+	vPos = vec3(view * model * vec4(aPos, 1.0));
+	vLightPos = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
 }
